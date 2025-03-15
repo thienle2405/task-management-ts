1751e6d2cec12e8e7cc326c217a8b426f98faaed
thienle2405 <dt07ethicalhcker@gmail.com>
2025-03-16 00:30:14 +0700

    Edit

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -7,7 +7,7 @@ router.get("/detail/:id", controller.detail);
 router.patch("/change-status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.post("/create", controller.create);
-// router.patch("/edit/:id", controller.edit);
+router.patch("/edit/:id", controller.edit);
 // router.delete("/delete/:id", controller.delete);
 
 const taskRoutes: Router = router;
