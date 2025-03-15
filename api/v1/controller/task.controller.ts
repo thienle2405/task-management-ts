656c385646a48cb5f97bca8ffe5e935821d1c973
thienle2405 <dt07ethicalhcker@gmail.com>
2025-03-15 22:34:02 +0700

    Update task.controller.ts
    query sort

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+  //Find
   interface Find {
     deleted: boolean;
     status?: string;
@@ -14,8 +15,18 @@ export const index = async (req: Request, res: Response) => {
   if (req.query.status) {
     find.status = req.query.status.toString();
   }
+  //End Find
 
-  const tasks = await Task.find(find);
+  //Sort
+
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey.toString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  //End Sort
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 };
