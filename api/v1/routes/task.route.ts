a925752adeb89bd5661424241a4772a1508825b1
thienle2405 <dt07ethicalhcker@gmail.com>
2025-03-16 00:27:47 +0700

    Add new

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -6,7 +6,7 @@ router.get("/", controller.index);
 router.get("/detail/:id", controller.detail);
 router.patch("/change-status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
-// router.post("/create", controller.create);
+router.post("/create", controller.create);
 // router.patch("/edit/:id", controller.edit);
 // router.delete("/delete/:id", controller.delete);
 
