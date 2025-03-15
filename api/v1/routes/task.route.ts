30ed84300b8e5d1a4a9e4836d9a6b79aa7a1ce1b
thienle2405 <dt07ethicalhcker@gmail.com>
2025-03-16 00:19:10 +0700

    Change multi

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -5,7 +5,7 @@ import * as controller from "../controller/task.controller";
 router.get("/", controller.index);
 router.get("/detail/:id", controller.detail);
 router.patch("/change-status/:id", controller.changeStatus);
-// router.patch("/change-multi", controller.changeMulti);
+router.patch("/change-multi", controller.changeMulti);
 // router.post("/create", controller.create);
 // router.patch("/edit/:id", controller.edit);
 // router.delete("/delete/:id", controller.delete);
