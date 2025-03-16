da3faa53b84d0c79fd1470e2adb54380dc6589c9
thienle2405 <dt07ethicalhcker@gmail.com>
2025-03-16 12:36:55 +0700

    add route cho user

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 const router: Router = express.Router();
 import * as controller from "../controller/task.controller";
+import * as authMiddleware from "../middlewares/auth.middleware";
 
 router.get("/", controller.index);
 router.get("/detail/:id", controller.detail);
